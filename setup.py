e3c826e907c685eaa020f08209e8aac52ade3a0c
Greg Fitch <greg.fitch@rice.edu>
2016-03-15 10:54:05 -0500

    packaging changes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='staxrail',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     scripts=[],
     # zip_safe=True,
